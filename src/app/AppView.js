b407d8fda41e1e99594e9efd5874bdb46b7ef50b
Brendan Briggs <br.br.cat@gmail.com>
2014-06-08 00:56:20 -0400

    A change in perspective

--- src/app/AppView.js
+++ src/app/AppView.js
@@ -1,19 +1,30 @@
 define(function(require, exports, module) {
   var View = require('famous/core/View');
   var Surface = require('famous/core/Surface');
+  var Modifier = require('famous/core/Modifier');
+  var Transform = require('famous/core/Transform');
   
   function AppView() {
     View.apply(this, arguments);
     var surface = new Surface({
-          size: [200, 200],
+          size: [undefined, undefined],
             content: "Hello World",
             classes: ["red-bg"],
             properties: {
-                      lineHeight: "200px",
-                textAlign: "center"
+                lineHeight: "200px",
+                textAlign: "center",
+                backgroundColor: "blue"
           }
     });
-    this._add(surface);
+    var rotateModifier = new Modifier({
+      transform : Transform.multiply(Transform.multiply(
+     //                 Transform.translate(window.innerWidth - 140, 0, 80),
+                      Transform.translate(0, 80, 0),
+                      Transform.rotateY(-1 * Math.PI/6)
+                      ), Transform.scale(0.6, 0.6, 1))
+    });
+        
+    this._add(rotateModifier).add(surface);
   }
 
   AppView.prototype = Object.create(View.prototype);
