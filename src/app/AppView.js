948e7175dd276475487d2dc0142f463dd8428c0d
Brendan Briggs <br.br.cat@gmail.com>
2014-06-08 08:59:01 -0400

    Structuring

--- src/app/AppView.js
+++ src/app/AppView.js
@@ -4,30 +4,12 @@ define(function(require, exports, module) {
   var Modifier = require('famous/core/Modifier');
   var Transform = require('famous/core/Transform');
   var StateModifier = require('famous/modifiers/StateModifier');
- 
+  var MarketView = require('./MarketView');
+
   function AppView() {
     View.apply(this, arguments);
-    var surface = new Surface({
-          size: [undefined, undefined],
-            content: "Hello World",
-            classes: ["red-bg"],
-            properties: {
-                lineHeight: "200px",
-                textAlign: "center",
-                backgroundColor: "blue"
-          }
-    });
-    var mod = new StateModifier({origin: [1,0.5]});
-    
-    surface.on('click',function(){
-      var transform = Transform.multiply(Transform.multiply(
-     //                 Transform.translate(window.innerWidth - 140, 0, 80),
-                      Transform.translate(0, 0, 0),
-                      Transform.rotateY(-1 * Math.PI/6)
-                      ), Transform.scale(0.9, 0.9, 1));
-      mod.setTransform(transform, { duration : 600, curve: 'easeOut' });
-    });
-     this._add(mod).add(surface);
+    var marketView = new MarketView();
+    this._add(marketView);
   }
 
   AppView.prototype = Object.create(View.prototype);
---
+++ src/app/MarketView.js
@@ -0,0 +1,37 @@
+define(function(require, exports, module) {
+  var View = require('famous/core/View');
+  var Surface = require('famous/core/Surface');
+  var Modifier = require('famous/core/Modifier');
+  var Transform = require('famous/core/Transform');
+  var StateModifier = require('famous/modifiers/StateModifier');
+ 
+  function MarketView() {
+    View.apply(this, arguments);
+    var mod = new StateModifier({origin: [1,0.5]});
+    var surface = new Surface({
+          size: [undefined, undefined],
+            content: "Hello World",
+            classes: ["red-bg"],
+            properties: {
+                lineHeight: "200px",
+                textAlign: "center",
+                backgroundColor: "blue"
+          }
+    });
+    
+    surface.on('click',function(){
+      var transform = Transform.multiply(Transform.multiply(
+     //                 Transform.translate(window.innerWidth - 140, 0, 80),
+                      Transform.translate(0, 0, 0),
+                      Transform.rotateY(-1 * Math.PI/6)
+                      ), Transform.scale(0.9, 0.9, 1));
+      mod.setTransform(transform, { duration : 600, curve: 'easeOut' });
+    });
+     this._add(mod).add(surface);
+  }
+
+  MarketView.prototype = Object.create(View.prototype);
+  MarketView.prototype.constructor = MarketView;
+
+  module.exports = MarketView;
+});
