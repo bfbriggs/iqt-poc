d22ea172edba88f2fc7355392de3fb3175d8ae63
Brendan Briggs <br.br.cat@gmail.com>
2014-06-11 16:21:58 -0400

    Create circle views based on imported MarketData

--- src/app/CircleView.js
+++ src/app/CircleView.js
@@ -4,11 +4,34 @@ define(function(require, exports, module){
   var Modifier = require('famous/core/Modifier');
   var Transform = require('famous/core/Transform');
   var StateModifier = require('famous/modifiers/StateModifier');
+  var MarketData = require('./data/MarketData');
 
   function CircleView() {
     View.apply(this, arguments);
-    this.centralAngle = 4 * Math.atan(this.csHeight/this.chordLength);
+    this.centralAngle = 4 * Math.atan(this.csHeight/(this.chordLength/2));
+    this.circleRadius = (this.chordLength/2)/Math.sin(this.centralAngle/2);
+    this.circumference = 2 * Math.PI * this.circleRadius;
+    this.arcLength = this.circumference * this.centralAngle/(2*Math.PI);
 
+    var offset;
+    // initialize bars
+    MarketData.map(function(item, idx){
+      return new BarView(barPos.call(this, idx, offset));
+    }.bind(this));
+  }
+
+  function barPos(idx, offset) {
+    var barOffset = this.barWidth + this.barGap;
+    var tinyAngle = 2 * Math.PI * barOffset/this.circumference;
+    var triAngle = Math.PI/2 - tinyAngle * (offset + idx);
+    var x = Math.cos(triAngle) * this.circleRadius;
+    var y = Math.sin(triAngle) * this.circleRadius;
+    var angle = tinyAngle * (offset + idx);
+    return {
+      x: x,
+      y: y, 
+      angle: angle
+    };
   }
 
   CircleView.prototype = Object.create(View.prototype);
@@ -17,7 +40,8 @@ define(function(require, exports, module){
   CircleView.DEFAULT_OPTIONS = {
     chordLength: 1000,
     csHeight: 200,
-    barGap: 30
+    barGap: 30,
+    barWidth: 20,
   };
 
 });
---
+++ src/app/data/MarketData.js
@@ -0,0 +1,14 @@
+define(function(require, exports, module) {
+  module.exports = [
+    {name: "Briefing", val: 1},
+    {name: "Markets", val: 2},
+    {name: "Lists", val: 3},
+    {name: "Workspaces", val: 4},
+    {name: "Libraries", val: 5},
+    {name: "Favorites", val: -1},
+    {name: "Add via Picture", val: -2},
+    {name: "Notifications", val: 0},
+    {name: "Help", val: 0.5},
+    {name: "Notifications", val: -0.5}
+  ];
+}):
