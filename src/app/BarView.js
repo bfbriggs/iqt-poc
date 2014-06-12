71321bb5d0c155b54d2282300090d5a17a16efa1
Brendan Briggs <br.br.cat@gmail.com>
2014-06-12 13:29:54 -0400

    Coordinate transforms should "theoretically work"

--- src/app/BarView.js
+++ src/app/BarView.js
@@ -5,8 +5,63 @@ define(function(require, exports, module) {
   var Transform = require('famous/core/Transform');
   var StateModifier = require('famous/modifiers/StateModifier');
 
-  function BarView() {
+  function BarView(options) {
     View.apply(this, arguments);
+    this.options = options;
+    var surface = createSurface.call(this, options.item);
+    this._add(surface);
+  }
+  
+
+
+  function createSurface(item){
+    var positive = (item.val > 0);
+    var gradient = (positive) ? this.positiveGradient : this.negativeGradient;
+    var surface = new Surface({
+      size: [20, 50],
+      backgroundImage: gradient
+    });
+
+    return surface;
+  }
+
+  BarView.prototype = Object.create(View.prototype);
+  BarView.prototype.constructor = BarView;
+  
+  // Return if in viewport (yes for now)
+  BarView.prototype.isVisible = function(){
+    return true;
+  };
+
+  // update position
+  BarView.prototype.updatePos = function(angleDelta){
+   var newAngle = this.getPos().angle + angleDelta;
+   var coords = pol2Car(newAngle, this.options.circle.circleRadius); 
+  
+   this.options.x = coords.x;
+   this.options.y = coords.y;
+   this.options.angle = newAngle;
+  };
+
+  BarView.prototype.getPos = function(){
+    return {
+      x: this.options.x,
+      y: this.options.y,
+      angle: this.options.angle
+    };
+  };
+
+  BarView.DEFAULT_OPTIONS = {
+    negativeGradient: "linear-gradient(to left,#500a14 0% ,#d91a36 100% )",
+    positiveGradient: "linear-gradient(to right,#034f5d 0% ,#07bcde 100% )"
+  };
+  
+  // angle is the central angle of the arc from the top of the circle to the specified point
+  function pol2Car(angle, radius){
+    return {
+      x: Math.cos(angle) * radius,
+      y: Math.sin(angle) * radius
+    };
   }
 
 });
--- src/app/CircleView.js
+++ src/app/CircleView.js
@@ -18,6 +18,7 @@ define(function(require, exports, module){
     this.bars = MarketData.map(function(item, idx){
       var barOpts = barPos.call(this, idx, offset);
       barOpts['data'] = item;
+      barOpts['circle'] = this;
       return new BarView(barOpts);
     }.bind(this));
   }
@@ -25,10 +26,11 @@ define(function(require, exports, module){
 
   function barPos(idx, offset) {
     var barOffset = this.barWidth + this.barGap;
+    //the angle between two bar centers
     var tinyAngle = 2 * Math.PI * barOffset / this.circumference;
     var triAngle = Math.PI/2 - tinyAngle * (offset + idx);
-    var x = Math.cos(triAngle) * this.circleRadius;
-    var y = Math.sin(triAngle) * this.circleRadius;
+    var x = pol2Car(triAngle, this.circleRadius).x; 
+    var y = pol2Car(triAngle, this.circleRadius).y; 
     var angle = tinyAngle * (offset + idx);
     return {
       x: x,
@@ -36,6 +38,20 @@ define(function(require, exports, module){
       angle: angle
     };
   }
+
+  // angle is the central angle of the arc from the top of the circle to the specified point
+  function pol2Car(angle, radius){
+    return {
+      x: Math.cos(angle) * radius,
+      y: Math.sin(angle) * radius
+    };
+  }
+
+  // linear function (not very accurate)
+  function offset2Ang(vect){
+    return Math.atan(vect.deltaX / this.circleRadius);
+  }
+
 
   CircleView.prototype = Object.create(View.prototype);
   CircleView.prototype.constructor = CircleView;
@@ -45,38 +61,48 @@ define(function(require, exports, module){
     csHeight: 200,
     barGap: 30,
     barWidth: 20,
-    dragMultiple:1
+    dragMultiple: 1
   };
 
 
   CircleView.prototype.getVisibleBars = function(){
     //convert to binary search
-    var searchArr = [];
-    return this.bars.filter(function(bar){
+    var searchArr = this.bars.filter(function(bar){
       return bar.isVisible();
     });
+    return {
+      arr: searchArr,
+      startOffset: this.bars.indexOf(searchArr[0]),
+      endOffset: this.bars.indexOf(searchArr[searchArr.length - 1])
+    };
   }
 
   // Set new positions of bars
+  // delta is expressed in x-delta for now...
   CircleView.prototype.updateBars = function(delta){
+    var angleDelta = offset2Ang.call(this, delta);
     //Grab slice that definitely needs to be update
     var visibleBarsObj = this.getVisibleBars();
     var visibleBars = visibleBarsObj.arr;
     var startOffset = visibleBarsObj.startOffset;
     var endOffset = visibleBarsObj.endOffset;
+    // this should be optimized
+    this.bars.map(function(bar){
+      bar.updatePos(angleDelta);
+    });   
 
-    //right
-    if (delta > 0){
-      this.getLeft(this.bars).concat(visibleBars).map(function(bar){
-        bar.updatePos(delta);
-      });   
-    }
-    //left
-    else if (delta < 0){
-      visibleBars.concat(this.getLeft(this.bars)).map(function(bar){
-        bar.updatePos(delta);
-      });   
-    }
+//    //right
+//    if (delta > 0){
+//      this.getLeft(this.bars).concat(visibleBars).map(function(bar){
+//        bar.updatePos(delta);
+//      });   
+//    }
+//    //left
+//    else if (delta < 0){
+//      visibleBars.concat(this.getLeft(this.bars)).map(function(bar){
+//        bar.updatePos(delta);
+//      });   
+//    }
   }
 
 
